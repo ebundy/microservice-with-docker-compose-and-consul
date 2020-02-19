4a84b910986e40e0a3df78c430560bb1de010963
david-dev <coucou@yahoo.fr>
2020-02-19 21:52:53 +0100

    remove no required deps, improve the loadbalancer trace

--- weather-forecast-service/src/main/java/davidxxx/microservicewithconsul/WeatherForecastController.java
+++ weather-forecast-service/src/main/java/davidxxx/microservicewithconsul/WeatherForecastController.java
@@ -1,11 +1,13 @@
 package davidxxx.microservicewithconsul;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Random;
@@ -14,10 +16,17 @@ import java.util.Random;
 @RequestMapping(value = "/forecast", produces = MediaType.TEXT_PLAIN_VALUE)
 public class WeatherForecastController {
 
+  private static final Logger logger = LoggerFactory.getLogger(WeatherForecastController.class);
+  private String instanceId;
+
+  public WeatherForecastController(@Value("${spring.cloud.consul.discovery.instanceId}") String instanceId){
+    this.instanceId = instanceId;
+  }
 
   @RequestMapping(method = RequestMethod.GET)
-  public ResponseEntity<String> get(HttpServletRequest request) {
-    System.out.println("PERFORMED BY : " + request.getServerName() + ":" + request.getServerPort());
+  public ResponseEntity<String> get() {
+//    logger.info("PERFORMED BY : " + request.getServerName() + ":" + request.getServerPort());
+    logger.info("get() from the " + instanceId + " instance");
     int randomInt = new Random().nextInt(2);
     if (randomInt == 0) {
       return ResponseEntity.ok("GOOD");
